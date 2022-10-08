bbfd6f93e762019ff8870b16f14feb6e54cfd27f
ChinasMr <974105109@qq.com>
2022-10-08 15:06:39 +0800

    feat: add new rtsp server options.

--- pkg/transport/rtsp/options.go
+++ pkg/transport/rtsp/options.go
@@ -1,6 +1,9 @@
 package rtsp
 
-import "github.com/ChinasMr/kaka/pkg/log"
+import (
+	"github.com/ChinasMr/kaka/pkg/log"
+	"time"
+)
 
 type ServerOption func(s *Server)
 
@@ -13,5 +16,29 @@ func Address(addr string) ServerOption {
 func Logger(logger log.Logger) ServerOption {
 	return func(s *Server) {
 		s.log = log.NewHelper(logger)
+	}
+}
+
+func Network(network string) ServerOption {
+	return func(s *Server) {
+		s.network = network
+	}
+}
+
+func Timeout(duration time.Duration) ServerOption {
+	return func(s *Server) {
+		s.timeout = duration
+	}
+}
+
+func RTP(rtp string) ServerOption {
+	return func(s *Server) {
+		s.rtp = rtp
+	}
+}
+
+func RTCP(rtcp string) ServerOption {
+	return func(s *Server) {
+		s.rtcp = rtcp
 	}
 }
